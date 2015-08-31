8f49ad2078c97ef17031d194ebfb87c6f57ec1d3
Sean Hagar <rcix2010@gmail.com>
2015-08-31 13:57:31 -0700

    cleaned up non working code in roguep.py and standardized variable naming therein

--- rogueP.py
+++ rogueP.py
@@ -3,8 +3,8 @@ from objects.attribute import Attribute, AttributeTag
 from objects.entity import Entity
 from behavior_manager import BehaviorManager
 
-SCREEN_WIDTH = 80
-SCREEN_HEIGHT = 50
+screen_width = 80
+screen_height = 50
 limit_fps = 10
 behavior_manager = BehaviorManager()
 entities = []
@@ -18,24 +18,9 @@ entities.append(
 		])
 	)
 
-
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'RogueP pre-dev', False)
+libtcod.console_init_root(screen_width, screen_height, 'RogueP pre-dev', False)
 libtcod.sys_set_fps(limit_fps)
 
-def handle_keys():
-	global playerx, playery
-	key = libtcod.console_check_for_keypress()
-	if key.vk == libtcod.KEY_ENTER and key.lalt:
-		libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
- 
-	elif key.vk == libtcod.KEY_ESCAPE:
-		return True  #exit game
-
 while not libtcod.console_is_window_closed():
-
 	behavior_manager.update_behaviors(entities)
-
-	exit = handle_keys()
-	if exit:
-		break
